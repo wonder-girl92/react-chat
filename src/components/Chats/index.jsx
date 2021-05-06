67440ab8c53c5d312034bebf25f8916178a4aa67
Razet <ismi_razet@mail.ru>
2021-05-07 00:18:25 +0300

    fetch json fullname

--- src/components/Chats/index.jsx
+++ src/components/Chats/index.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
-import styles from './chats.module.css'
+import React, { useEffect } from 'react';
+import styles from './chats.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { loadContacts } from '../../redux/ducks/contacts';
+import Search from './Search';
+import Skeleton from './Skeleton';
+import Contact from './Contact';
 
 function Chats(props) {
+  const dispatch = useDispatch();
+  const contacts = useSelector((state) => state.contacts.items);
+  const loading = useSelector((state) => state.contacts.loading);
+
+  useEffect(() => {
+    dispatch(loadContacts());
+  }, []);
+
   return (
     <div className={styles.chats}>
-      Разят Блок
+      <div className={styles['contacts-search']}>
+        <input />
+        <Search />
+      </div>
+
+      <div className={styles['contacts-chats']}>
+        {loading ? (
+          <Skeleton />
+        ) : (
+          contacts.map((contact) => {
+            return <Contact key={contact._id} contact={contact} />;
+          })
+        )}
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-import reportWebVitals from "./reportWebVitals";
-import { Provider } from "react-redux";
-import { store } from "./redux/configureStore";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { store } from './redux/configureStore';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
-
 
 reportWebVitals();
