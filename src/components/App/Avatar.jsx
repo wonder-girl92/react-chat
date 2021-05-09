ad7366fcdc943046d2d8eb2776a07e7473fa8c1c
baimag <baimag02@yandex.ru>
2021-05-09 14:49:31 +0300

    avatar/baimag

--- src/components/App/Avatar.jsx
+++ src/components/App/Avatar.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
-
+import styles from './app.module.css';
+import PropTypes from "prop-types"
+import { useSelector } from 'react-redux';
 function Avatar(props) {
+  const prf = useSelector(state => state.application.profile)
   return (
-    <div>
-
+    <div
+      className={`${styles.avatar} ${styles[props.size]}`}
+      style={{ margin: props.margin }}
+    >
+      {props.children}
+      {/*{prf.fullname.substr[0]}*/}
+      ERR
     </div>
   );
 }
+
+Avatar.propTypes = {
+  children: PropTypes.node.isRequired,
+  size: PropTypes.string.isRequired,
+  margin: PropTypes.string,
+};
 
 export default Avatar;
--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -7,27 +7,20 @@ import Media from './UserLow/Media';
 import { loadProfile } from '../../redux/ducks/application';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { logger } from 'redux-logger/src';
+
 
 function Profile(props) {
-   const id = useParams().id
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(loadProfile());
   }, []);
   const profile = useSelector((state) => state.application.profile);
   const load = useSelector(state => state.application.loading)
-  const users = useSelector(state => state.contacts.items)
-  const SearchUsers = users.find((user) =>
-    id === user._id
-  )
-  // console.log(SearchUsers);
-  // if (SearchUsers === undefined) {
-  //   return null;
-  // }
+  const id = useParams().id;
+
   return (
         <div className={style.profile}>
-          <UserInfo prf={profile}/>
+          <UserInfo prf={profile} />
           <ConnectionUser />
           <SocialMedia prf={profile}/>
           <Media />
